fdfbac56801331a3db1f14899d81481b6115cd96
Preslav Rachev <rachev.preslav@gmail.com>
2017-08-09 22:01:14 +0200

    #1 Adds a simple controller for displaying the charting data results

---
+++ src/main/kotlin/com/preslavrachev/cryptotrader/mvc/controller/DashboardController.kt
@@ -0,0 +1,21 @@
+package com.preslavrachev.cryptotrader.mvc.controller
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody
+import remote.poloniex.service.PoloniexApiService
+import javax.inject.Inject
+
+@Controller
+@RequestMapping("/")
+class DashboardController {
+
+    @Inject
+    lateinit var poloniexApiService: PoloniexApiService
+
+    @RequestMapping("hello")
+    @ResponseBody
+    fun hello(): PoloniexApiService.ChartDataEntryList {
+        return poloniexApiService.getChartData()
+    }
+
+}
