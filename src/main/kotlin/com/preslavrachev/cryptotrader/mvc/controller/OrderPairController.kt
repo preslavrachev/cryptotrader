d85a88f6367a32f329d497965e3324addf530d29
Preslav Rachev <rachev.preslav@gmail.com>
2018-02-15 06:33:37 +0100

    Prepares a controller with one action

---
+++ src/main/kotlin/com/preslavrachev/cryptotrader/mvc/controller/OrderPairController.kt
@@ -0,0 +1,41 @@
+package com.preslavrachev.cryptotrader.mvc.controller
+
+import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonValue
+import com.preslavrachev.cryptotrader.mvc.controller.PairType.BUY_SELL
+import org.springframework.web.bind.annotation.*
+
+enum class PairType(@JsonValue val id: Long) {
+    BUY_SELL(id = 1L),
+    SELL_BUY(id = 2L)
+    ;
+
+    companion object {
+        /**
+         * JSON creator
+         * NOTE: when giving the creator function parameter the same name as the underlying property we're about to
+         * check, the method would not work. Changing the name (to @{link requestId} in this case) seems to have done the job
+         *
+         * @see <a href="https://github.com/FasterXML/jackson-module-kotlin/issues/75" />
+         * @see <a href="https://github.com/FasterXML/jackson-module-kotlin/issues/78" />
+         */
+        @JsonCreator
+        @JvmStatic
+        fun fromId(requestId: Long): PairType {
+            return values().first { it.id == requestId }
+        }
+    }
+}
+
+data class OrderPairWebRequest(val type: PairType = BUY_SELL, val quoteAmount: Float)
+
+@RestController
+@RequestMapping("/orderPair")
+class OrderPairController {
+
+    @PostMapping("/{tickerPair}")
+    fun placeOrderPair(@PathVariable(name = "tickerPair") tickerPair: String, @RequestBody request: OrderPairWebRequest): Any {
+        return request
+    }
+
+}
